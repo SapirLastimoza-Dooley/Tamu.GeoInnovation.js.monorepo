4c4636e21f95a250f7677295ad509d97da7c8cf0
Edgar Hernandez <imacleopard2010@gmail.com>
2020-03-06 20:01:20 -0600

    Working on interactive service components

--- libs/geoservices/public/src/lib/pages/services/pages/geocoding/geocoding.component.ts
+++ libs/geoservices/public/src/lib/pages/services/pages/geocoding/geocoding.component.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 import { Geocoder } from '@tamu-gisc/common/utils/geometry/geoprocessing';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tamu-gisc-geocoding',
@@ -13,7 +14,9 @@ export class GeocodingComponent implements OnInit {
   private geocode: Geocoder;
   public result: Observable<string>;
 
-  constructor() {
+  public geocodeForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
     this.geocode = new Geocoder({
       apiKey: 'demo',
       version: '4.01',
@@ -26,5 +29,18 @@ export class GeocodingComponent implements OnInit {
     this.result = this.geocode.execute().pipe(switchMap((r) => of(JSON.stringify(r, null, '   '))));
   }
 
-  public ngOnInit() {}
+  public ngOnInit() {
+    this.geocodeForm = this.fb.group({
+      streetAddress: ['', Validators.required],
+      city: [''],
+      state: [''],
+      zip: ['']
+    });
+  }
+
+  public test(e) {
+    const v = this.geocodeForm.getRawValue();
+
+    debugger;
+  }
 }

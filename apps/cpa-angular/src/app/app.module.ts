d9cdae30ba4a2ee47423fdf5d7bad3e005ace836
Edher <imacleopard2010@gmail.com>
2020-08-20 15:01:47 -0500

    Fixed HttpClient injector error
    HttpClientModule was not imported in the root module.

--- apps/cpa-angular/src/app/app.module.ts
+++ apps/cpa-angular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
@@ -30,6 +31,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
     EnvironmentModule,
     LocalStoreModule,

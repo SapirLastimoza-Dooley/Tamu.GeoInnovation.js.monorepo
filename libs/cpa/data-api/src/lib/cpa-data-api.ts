6a9dcaeeed9bf50d960d20226bc4c90c384e6fb3
Edher <imacleopard2010@gmail.com>
2020-11-03 06:11:31 -0600

    Added basic live layers functionality

--- libs/cpa/data-api/src/lib/cpa-data-api.ts
+++ libs/cpa/data-api/src/lib/cpa-data-api.ts
@@ -1,3 +1,4 @@
 export * from './modules/responses/responses.module';
 export * from './modules/scenarios/scenarios.module';
 export * from './modules/workshops/workshops.module';
+export * from './modules/layers/layers.module';
